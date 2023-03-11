aef2eed48c4a6fd68ea062742d10e2785d765dbe
UncleAndrewSedov <123667974+UncleAndrewSedov@users.noreply.github.com>
2023-03-11 18:47:49 +0400

    Add files via upload

---
+++ MyApplication2/app/src/main/java/com/example/myapplication2/SecondActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       //setContentView(R.layout.activity_second);
+        TextView textView = new TextView(this);
+        textView.setTextSize(20);
+        textView.setPadding(16, 16, 16, 16);
+        textView.setText("SecondActivity");
+        setContentView(textView);
+
+    }
+}
